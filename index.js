e02fbedf55d7f399b6a4b81f8a93c766122420d2
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-05-02 09:45:39 -0400

    api now pulls github username and has a separate const for the avatar

--- index.js
+++ index.js
@@ -7,63 +7,72 @@ const api = require("./utils/api")
 
 
 // Questions User Answers
-inquirer.prompt([
-  {
-      type: "input",
-      message: "What is your user GitHub username?",
-      name: "username"
-    },
+function askQuestions() {
+  return inquirer.prompt([
     {
-      type: "input",
-      message: "What is the title of your project?",
-      name: "title"
-    },
-    {
-      type: "input",
-      message: "Please provide a description of your project.",
-      name: "description"
-    },
-    {
-      type: "input",
-      message: "Please a table of contents for your project.",
-      name: "table of contents"
-    },
-    {
-      type: "input",
-      message: "Please provide the steps required to install your project.",
-      name: "installation"
-    },
-    {
-      type: "input",
-      message: "Please provide an example of how your project can be used.",
-      name: "usage"
-    },
-    {
-      type: "input",
-      message: "What king of license would you like to have?",
-      name: "license"
-    },
-    {
-      type: "input",
-      message: "Including yourself, please list out all contributors",
-      name: "contributing"
-    },
-    {
-      type: "input",
-      message: "Have you tested your project",
-      name: "tests"
-    }
-  ]).then(function(data) {
-    fs.writeFile("README.md", JSON.stringify(data, null, '\t'), function(err) {
-  
-  if (err) {
+        type: "input",
+        message: "What is your user GitHub username?",
+        name: "username"
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+      },
+      {
+        type: "input",
+        message: "Please provide a description of your project.",
+        name: "description"
+      },
+      {
+        type: "input",
+        message: "Please a table of contents for your project.",
+        name: "table of contents"
+      },
+      {
+        type: "input",
+        message: "Please provide the steps required to install your project.",
+        name: "installation"
+      },
+      {
+        type: "input",
+        message: "Please provide an example of how your project can be used.",
+        name: "usage"
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "What king of license would you like to have?",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+      },
+      {
+        type: "input",
+        message: "Including yourself, please list out all contributors",
+        name: "contributing"
+      },
+      {
+        type: "input",
+        message: "Have you tested your project",
+        name: "tests"
+      }
+    ]); // end of return 
+  }; // end of function 
+
+// write the answers to a new README file
+function writeFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
     return console.log(err);
-  }
-
-  console.log("Success!");
-
+    }
+    console.log("successfully wrote README file");
   });
-});
+}
    
 
     
--- utils/api.js
+++ utils/api.js
@@ -1,23 +1,32 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
 
-// grabs the github avatar image 
+// Prompt user for GitHub details 
 const api = {
   getUser(username) {
     inquirer
     .prompt({
       message: "What is your user GitHub username?",
       name: "username"
-    })
+    }) 
+
+    // Grab the GitHub UserName Details from the source 
     .then(function({ username }) {
-      const queryUrl = `https://api.github.com/users/${username}/avatar_url;`;
-    })
+      const queryUrl = `https://api.github.com/users/${username}`;
+
+      // Grab the GitHub avatar image
+      axios.get(queryUrl).then(function(res) {
+
+        const avatar = res.data.avatar_url
+        console.log(avatar);
+      });  
+    });    
   }
 };
 
 // Export data in 'const api' to the index.js page 
 module.exports = api;
 
-//https://api.github.com/users/tiffanycasey
+
 
 
